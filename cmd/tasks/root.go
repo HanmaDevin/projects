5a35026c9b10e17474a02328b7572b32b8aed96c
HanmaDevin <devinbc14@gmail.com>
2025-05-03 19:04:17 +0200

    Revert "moved to folder"
    This reverts commit 8ec1ba19bece437cff3fc4e42f28fa40fb9f2454.

---
+++ cmd/tasks/root.go
@@ -0,0 +1,21 @@
+package tasks
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "tasks",
+	Short: "tasks - a simple CLI to-do manager",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. :( There was an error: %s", err)
+		os.Exit(1)
+	}
+}
