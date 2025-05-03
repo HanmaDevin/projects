1f45f38e40d9c5d4845496a14ab3502a8a771d8d
HanmaDevin <devinbc14@gmail.com>
2025-05-03 19:50:21 +0200

    added tool preperation with pseudo database

--- go_projects/main.go
+++ go_projects/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "Go_Projects/todo_list/cmd/tasks"
+import (
+	"Go_Projects/todo_list/cmd/tasks"
+	"os"
+)
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
+	homeDir, err := os.UserHomeDir()
+	check(err)
+
+	dir := homeDir + "\\.go_data"
+	filename := homeDir + "\\.go_data\\tasks.csv"
+	header := "ID,Description,CreatedAt,IsComplete"
+
+	if !exists(filename) {
+		_, err := os.Create(filename)
+		check(err)
+	} else if !exists(dir) {
+		err := os.Mkdir(dir, 0755)
+		check(err)
+	}
+
+	os.WriteFile(filename, []byte(header), 0644)
+
 	tasks.Execute()
 }
