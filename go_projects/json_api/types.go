5554474b0dad243b44d8576fe8383f95427393e2
HanmaDevin <devinbc14@gmail.com>
2025-05-21 20:06:43 +0200

    fixed json tags

--- go_projects/json_api/types.go
+++ go_projects/json_api/types.go
@@ -1,20 +1,29 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
+
+type CreateAccountRequest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
 
 type Account struct {
-	ID         int    `json: "id"`
-	FirstName  string `json: "first_name"`
-	LastName   string `json: "last_name"`
-	BankNumber int64  `json: "bank_number"`
-	Balance    int64  `json: "balance"`
+	ID         int       `json:"id"`
+	FirstName  string    `json:"first_name"`
+	LastName   string    `json:"last_name"`
+	BankNumber int64     `json:"bank_number"`
+	Balance    int64     `json:"balance"`
+	CreatedAt  time.Time `json:"created_at"`
 }
 
 func NewAccount(firstname, lastname string) *Account {
 	return &Account{
-		ID:         rand.Intn(10000),
 		FirstName:  firstname,
 		LastName:   lastname,
 		BankNumber: int64(rand.Intn(1000000)),
+		CreatedAt:  time.Now().UTC(),
 	}
 }
